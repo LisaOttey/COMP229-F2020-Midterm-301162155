c6b7fbc18626d6b7b3eb45360a8b39dbfa8b4f72
LisaOttey <71458542+LisaOttey@users.noreply.github.com>
2021-03-03 21:04:04 -0500

    Add files via upload

---
+++ server/config/db.js
@@ -0,0 +1,6 @@
+module.exports = 
+{
+   // "URI":"mongodb://localhost/books"
+    "URI": "mongodb+srv://LisaO:EOBVSqx81HYISOtC@cluster0.zgcrn.mongodb.net/book_store?retryWrites=true&w=majority"
+    
+}
